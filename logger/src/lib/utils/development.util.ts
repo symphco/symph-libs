7c1cd630e628159ffd920bc839dba777b47c55a1
NIKKO BRYAN DELOS REYES <102723898+nik-symph@users.noreply.github.com>
2024-04-09 11:13:39 +0800

    [Feat][Nik] Implement dynamic cloud service support

--- logger/src/lib/utils/development.util.ts
+++ logger/src/lib/utils/development.util.ts
@@ -1,15 +1,10 @@
-import { NextFunction } from 'express';
 import * as winston from 'winston';
 import { LoggerService } from '../services/logger.service';
 
 export function createDevelopmentLogger() {
   const logger = LoggerService.createLogger([new winston.transports.Console()]);
 
-  return (
-    req: Request & { log: winston.Logger },
-    res: Response,
-    next: NextFunction
-  ) => {
+  return (req: any, res: any, next: any) => {
     req.log = logger;
     next();
   };
