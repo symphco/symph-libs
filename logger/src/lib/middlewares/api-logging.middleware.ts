c124c7223e73241aace9dbae33a5a32771896ea2
loewe <loewe.alivio@symph.co>
2024-05-27 17:19:39 +0800

    [Feature][Loewe] Scrube sensitive information upon logging

--- logger/src/lib/middlewares/api-logging.middleware.ts
+++ logger/src/lib/middlewares/api-logging.middleware.ts
@@ -1,7 +1,9 @@
 import { scrub, findSensitiveValues } from '@zapier/secret-scrubber';
+
 import { NextFunction, Request, Response } from 'express';
 
 import { LoggerService } from '../services/logger.service';
+import { recurseExtract } from '@zapier/secret-scrubber/lib/utils';
 
 const loggerService = new LoggerService();
 
@@ -24,49 +26,70 @@ function captureResponseBody(res: Response, callback: (body: any) => void) {
   res.on('finish', () => callback(responseBody));
 }
 
-export function apiLoggingMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const request = {
-    method: req.method,
-    path: req.path,
-    headers: req.headers,
-    payload: req.body,
-  };
-
-  const sensitiveValues = findSensitiveValues(request);
-  if (sensitiveValues.length > 0) {
-    loggerService.info('[REQUEST]', scrub(request, sensitiveValues));
-  } else {
-    loggerService.info('[REQUEST]', request);
-  }
-
-  captureResponseBody(res, (responseBody = {}) => {
-    const response = {
-      statusCode: res.statusCode,
-      statusMessage: res.statusMessage,
-      headers: res.getHeaders(),
-      body: responseBody?.toString(),
+export function apiLoggingMiddleware(sensitiveKeys: string[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const request = {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      body: req.body,
     };
 
-    const sensitiveValues = findSensitiveValues(response);
+    const sensitiveValues = extractSensitiveValues(request, sensitiveKeys);
 
-    if (sensitiveValues.length) {
-      loggerService.info('[RESPONSE]', scrub(response, sensitiveValues));
-    } else {
-      loggerService.info('[RESPONSE]', response);
-    }
-  });
-
-  res.on('error', (err: any) => {
-    loggerService.error('[ERROR]', {
-      message: err.message,
-      stack: err.stack,
-      statusCode: res.statusCode,
+    logAndScrub({
+      sensitiveValues,
+      data: request,
+      logLabel: '[REQUEST]',
     });
+
+    captureResponseBody(res, (responseBody) => {
+      const response = {
+        statusCode: res.statusCode,
+        statusMessage: res.statusMessage,
+        headers: res.getHeaders(),
+        body: responseBody
+          ? JSON.parse(responseBody.toString() || '{}')
+          : undefined,
+      };
+
+      const sensitiveValues = extractSensitiveValues(response, sensitiveKeys);
+
+      logAndScrub({
+        sensitiveValues,
+        data: response,
+        logLabel: '[RESPONSE]',
+      });
+    });
+
+    res.on('error', (err: any) => {
+      loggerService.error('[ERROR]', {
+        message: err.message,
+        stack: err.stack,
+        statusCode: res.statusCode,
+      });
+    });
+
+    next();
+  };
+}
+
+function logAndScrub(props: LogAndScrubType) {
+  const { data, logLabel, sensitiveValues } = props;
+
+  if (sensitiveValues.length > 0) {
+    loggerService.info(logLabel, scrub(data, sensitiveValues));
+  } else {
+    loggerService.info(logLabel, data);
+  }
+}
+
+function extractSensitiveValues(data: any, sensitiveKeys: string[]) {
+  const sensitiveKeysSet = new Set(sensitiveKeys);
+  const foundByScrubber = findSensitiveValues(data);
+  const foundByRecurseExtract = recurseExtract(data, (key: string) => {
+    return sensitiveKeysSet.has(key);
   });
 
-  next();
+  return [...new Set([...foundByScrubber, ...foundByRecurseExtract])];
 }
